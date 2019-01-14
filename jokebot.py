f4a153aa20d64c69bf1d0171b2fa929be6b8d4fa
MrunalPuram <mrunal.puram@gmail.com>
2019-01-13 20:14:32 -0800

    added check for csv format, commented code

--- jokebot.py
+++ jokebot.py
@@ -4,40 +4,48 @@ import sys
 import requests
 import json
 
+# Prints prompt, waits 2 seconds, prints punchline
 def deliver(prompt, punchline):
     print(prompt)
     time.sleep(2)
     print(punchline)
 
-# deliver("Hello", "World")
-
+# Reads user input on whether to proceed or exit
 def read_input():
-    user_input = input("'next' or 'quit'?\n")
+    user_input = input("--- 'next' or 'quit'? ---\n--> ")
     if user_input == "next":
         return True
     elif user_input == "quit":
         return False
     else:
-        print("I don't understand, please input 'next' or 'quit'")
+        print("--- I don't understand, please input 'next' or 'quit' ---")
         return read_input()
 
-
-# read_input()
-
-def read_joke(file_name):
-    with open(file_name, 'r') as f:
+# Reads CSV file and compiles list of jokes
+def read_csv(file_name):
+    try:
+        f = open(file_name, 'r')
+    except IOError:
+        print("--- Could not read file, fetching jokes from Reddit ---")
+        return read_reddit()
+    with f:
         reader = csv.reader(f)
-        joke_list = list(reader)
+        # joke_list = list(reader)
+        joke_list = []
+        for row in reader:
+            # Check if row has exactly 2 entries
+            if len(row) == 2:
+                joke_list.append(row)
     return joke_list
 
+# Compiles jokes from Reddit posts on /r/dadjokes
 def read_reddit():
     r = requests.get('https://www.reddit.com/r/dadjokes.json', headers={'User-agent': 'jokebot'})
-    jokes = r.json()
     clean_jokes = []
     json_data = json.loads(r.text)
-    # print(json_data)
-    # print(jokes)
     question_list = ["Why", "What", "How"]
+
+    # Filter json data and collect joke title and body
     for joke in json_data["data"]["children"]:
         if not joke["data"]["over_18"] and joke["data"]["selftext"]:
             first_word = joke["data"]["title"].split(' ')[0]
@@ -45,19 +53,18 @@ def read_reddit():
                 clean_jokes.append([joke["data"]["title"], joke["data"]["selftext"]])
     return clean_jokes
 
-# read_reddit()
 
 def main():
     if (len(sys.argv) > 1):
         file_name = sys.argv[1]
-        jokes_list = read_joke(file_name)
+        jokes_list = read_csv(file_name)
     else:
         jokes_list = read_reddit()
     for joke in jokes_list:
         deliver(joke[0], joke[1])
         if not read_input():
             exit()
-    print("We're out of jokes!")
+    print("--- We're out of jokes! ---")
     exit()
 
 if __name__ == '__main__':
